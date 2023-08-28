28a0403b8d3eb5d9992478cbd20cbf0345bf824c
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-28 23:37:28 +0530

    Update AQ3.c

--- AQ3.c
+++ AQ3.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
+
 void main()
-{int n,a,div=0;
+
+{
+int n,a,div=0;
 printf("enter the number");
 scanf("%d",&n);
+
 for(a=1;a<=n;a++)
 {if(n%a==0)
 div++;
 }
+
 if(div>2)
 printf("%d is not a prime number",n);
+
 else
 printf("%d is a prime number:",n);
+
 }
 
