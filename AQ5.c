a62cc83172f5b0bcb995861e2cdc43c3a5a5439f
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-09-01 18:40:51 +0530

    Update AQ5.c

--- AQ5.c
+++ AQ5.c
@@ -1,16 +1,28 @@
 #include<stdio.h>
 #include<conio.h>
+
 int main()
-{int a[55],i,n;
+{
+int a[55],i,n;
 float sum=0,average;
+
 printf("enter the number of students:");
 scanf("%d",&n);
+
 for(i=0;i<n;i++)
-{printf("enter marks of student:");
-scanf("%d",&a[i]);}
+{
+printf("enter marks of student:");
+scanf("%d",&a[i]);
+}
+
 for(i=0;i<n;i++)
-{sum=sum + a[i];}
+{
+sum=sum + a[i];
+}
+
 average=sum/n;
 printf("average marks of student: %f",average);
-return 0;}
+
+return 0;
+}
 
