3db05e0f943a95321f6e6671052de8694a8eb6ef
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-09-02 22:48:26 +0530

    Update AQ2.c

--- AQ2.c
+++ AQ2.c
@@ -1,19 +1,28 @@
 
 #include<stdio.h>
 #include<conio.h>
+
 void main()
-{ int x,y,z;
+
+{ 
+int x,y,z;
   printf("enter the first number");
   scanf("%d",&x);
+
   printf("\nenter the second number");
   scanf("%d",&y);
+
   printf("\nenter the third number");
   scanf("%d",&z);
+
   if (x>y && x>z)
   printf("%d is the greatest number",x);
+
   else if (y>x && y>z)
   printf("%d is the greatest number",y);
+
   else
   printf("%d is the greatest number",z);
+
 }
 
