b9e13ada61352b5bf919d69cbfba410b5cd4eede
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-09-05 20:59:23 +0530

    Update AQ6.c

--- AQ6.c
+++ AQ6.c
@@ -1,35 +1,52 @@
 #include<stdio.h>
 #include<conio.h>
+
 int main()         
-{int a[]={30,40,63,76,12},i,num,flag=0,index,temp,j;
+{
+int a[]={30,40,63,76,12},i,num,flag=0,index,temp,j;
+
 printf("enter the number you want to check:");
 scanf("%d",&num);
+
 for(i=0;i<5;i++)
 {
 if(num==a[i])
 flag=1;
 index=i;
 }
+
 if(flag==1)
+
 printf("%d is present in array at index %d",num,index);
+
 else
+
 printf("Number is not present in array");
+
 for(i=0;i<5;i++)
 {
+
 for(j=i+1;j<5;j++)
 {
+
 if(a[i] > a[j])
 {
+
 temp=a[i];
 a[i]=a[j];
 a[j]=temp;
+
          }
    }
 }
+
 printf("\n Arrays elements in ascending order:");
+
 for(i=0;i<5;i++)
 {
+
 printf("\n%d",a[i]);
+
 }
 }
 
