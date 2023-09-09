2c27f394eb7e2f2e98602163474d204e9b5e1a04
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-09-09 22:43:08 +0530

    Update AQ11.c

--- AQ11.c
+++ AQ11.c
@@ -1,28 +1,38 @@
 #include<stdio.h>
 #include<conio.h>
+
 int fact(int n);
 int fibo();
+
 int main()
-{int option;
+{
+int option;
 int f,n;
 int m,i,p;
+
 do{
 printf("1.factorial using recursion.\n2.fibonacci series using recursion.\n3.exit.\n");
 scanf("%d",&option);
+
 switch(option)
-{case 1:
+{
+case 1:
 printf("\n enter number to find factorial:");
 scanf("%d",&n);
 f= fact(n);
 printf("\n value of factorial is=%d\n",f);
 break; 
+
 case 2:
 printf("\n enter the limit of fiooonaci series:");
 scanf("%d",&m);
-for(i=0;i<=m;i++){
+
+for(i=0;i<=m;i++)
+{
 p=fibo(i);
 printf("\n %d",p);}
 break;
+
 case 3:
 break;
 default:
